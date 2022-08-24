33f01d511ec60c3383526699ed3d702c02d1b4d6
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-25 00:56:15 +1000

    code along done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [firstName, setFirstName] = useState("Eddie");
+  const [lastName, setLastName] = useState("Holmberg");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -11,13 +12,55 @@ function Form() {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+  
+  const [errors,setErrors] =useState([]);
+  const displayDataArray = [];
+  function handleSubmit(event){
+    event.preventDefault();
+    if(firstName){
+    const formData = {
+      firstName : firstName,
+      lastName : lastName
+    };
+    const dataArray = [...submittedData, formData]
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+    setErrors([])
+    displayDataArray.push(...dataArray)
+    console.log(displayDataArray)} 
+    
+    else {
+      setErrors(["First name is required!"])
+    }
+  }
+
+    const listOfSubmissions = submittedData.map((data,index)=>{
+      return(
+        <div key={index}>
+          {data.firstName} {data.lastName}
+        </div>
+      )
+    })
 
   return (
-    <form>
+    <div>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    {errors.length > 0 ?
+    errors.map((error, index)=>(
+      <p key={index} style={{color:"red"}}>{error}</p>
+    )): null
+  }
+    <h3>Submissions</h3>
+    {displayDataArray > 0 ? 
+    displayDataArray.map((name, index)=>{
+      <p key={index} style={{color:"green"}}>{name}</p>
+    }):null}
+    </div>
   );
 }
 
